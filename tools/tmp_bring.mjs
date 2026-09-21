d1987e481cb8e0c7a0194869f25f731ab621d351
Jeyson <jeyson@local>
2026-09-21 07:20:09 -0600

    Drop debug scratch files that a git add -A swept in

--- tools/tmp_bring.mjs
+++
@@ -1,7 +0,0 @@
-import { attach } from './cdp.mjs';
-const ts = await (await fetch('http://127.0.0.1:9333/json')).json();
-const ext = ts.find(t => t.url === 'chrome://extensions/');
-const cdp = await attach(9333, ext.id);
-await cdp.send('Page.bringToFront');
-console.log('brought front');
-process.exit(0);
